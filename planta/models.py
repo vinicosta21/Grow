4b96526bbc59e745a17a4234e4e884f458e64117
Bernardo Rocco <bernardo@berocco.com.br>
2021-07-14 00:17:41 -0300

    parametros

--- planta/models.py
+++ planta/models.py
@@ -3,15 +3,15 @@ from django.contrib.postgres.fields import ArrayField
 
 class Especie(models.Model):
     nome = models.CharField(max_length=200)
-    iluminacao = models.IntegerField()
-    umidade = models.IntegerField()
+    iluminacao = models.IntegerField(choices=[(0,"Baixa"), (1, "Média"), (2, "Alta")])
+    umidade = models.IntegerField(choices=[(0,"Baixa"), (1, "Média"), (2, "Alta")])
     def __str__(self):
         return self.nome
 
 class Planta(models.Model):
     serial = models.CharField(max_length=200, unique=True)
-    umi_manual = models.IntegerField(null=True, blank=True)
-    ilu_manual = models.IntegerField(null=True, blank=True)
+    umi_manual = models.IntegerField(choices=[(0,"Baixa"), (1, "Média"), (2, "Alta")], null=True, blank=True)
+    ilu_manual = models.IntegerField(choices=[(0,"Baixa"), (1, "Média"), (2, "Alta")], null=True, blank=True)
     especie = models.ForeignKey(Especie, related_name="epecie_plantada", on_delete=models.PROTECT, blank=True, null=True)
     ilu_dados = ArrayField(models.IntegerField(), blank=True)
     umi_dados = ArrayField(models.IntegerField(), blank=True)
@@ -22,7 +22,13 @@ class Planta(models.Model):
     
     def dados(self):
         if self.especie:
-            return (self.especie.iluminacao, self.especie.umidade)
+            ilu = self.especie.iluminacao
+            umi = self.especie.umidade
         else:
-            return (self.ilu_manual, self.umi_manual)
+            ilu = self.ilu_manual
+            umi = self.umi_manual
+        ilu_dic = {0:750, 1:2000, 2:2000}
+        hrs_dic = {0:8, 1:12, 2:16}
+        umi_dic = {0:3300, 1:2500, 2:1500}
+        return [hrs_dic[ilu], ilu_dic[ilu], umi_dic[umi]]
         
--- planta/views.py
+++ planta/views.py
@@ -10,26 +10,26 @@ def visaoPlanta(request):
     planta = Planta.objects.filter()[0]
     if request.method == 'POST':
         espData = request.POST['especie']
-        lumData = request.POST['luminosidade']
-        umiData = request.POST['umidade']
-        print('a', espData, 'b', lumData,'c', umiData)
-
         if espData!='0':
             planta.ilu_manual = None
             planta.umi_manual = None
             planta.especie = Especie.objects.filter(id=int(espData))[0]
             planta.save()
         else:
+            lumData = request.POST['luminosidade']
+            umiData = request.POST['umidade']
             planta.ilu_manual = int(lumData)
             planta.umi_manual = int(umiData)
             planta.especie = None
             planta.save()
     especies = Especie.objects.filter()
-    (ilu, umi) = planta.dados()
+    hrs, ilu, umi = planta.dados()
     ts = []
     for t in planta.ts_dados:
-        ts.append(t.timestamp()*1000)
-    context = {'ilu_ideal':ilu, "umi_ideal":umi, "especies":especies, "ilu_dados":planta.ilu_dados, "umi_dados":planta.umi_dados, "ts_dados":ts}
+        tst = t.timestamp()*1000
+        if tst >= timezone.now().timestamp()*1000-(24*60*60*1000):
+            ts.append(tst)
+    context = {'ilu_ideal':ilu, "umi_ideal":umi, "especies":especies, "ilu_dados":planta.ilu_dados[-len(ts):], "umi_dados":planta.umi_dados[-len(ts):], "ts_dados":ts}
     return render(request, template_name, context)
 
 @csrf_exempt
@@ -38,8 +38,8 @@ def dadosPlantas(request):
             data = request.body.decode('utf-8').split(',')
             print(data)
             planta = Planta.objects.filter(serial=data[0]).first()
-            (ilu, umi) = planta.dados()
-            respData = str(ilu)+','+str(umi)
+            (hrs, ilu, umi) = planta.dados()
+            respData = str(hrs)+','+str(ilu)+','+str(umi)
             planta.ilu_dados.append(data[1])
             planta.umi_dados.append(data[2])
             planta.ts_dados.append(timezone.now())
